6de8aee0a7e0487ce94f7057ee28f9f770ea3e12
vocaltech <it@vocaltech.fr>
2023-12-17 15:08:47 +0100

    Add some comments

--- src/main/java/fr/vocaltech/hibernate/HibernateApp.java
+++ src/main/java/fr/vocaltech/hibernate/HibernateApp.java
@@ -8,15 +8,19 @@ import org.hibernate.Transaction;
 
 public class HibernateApp {
     public static void main(String[] args) {
+        // Open a SessionFactory
         SessionFactory sf = HibernateUtils.getSessionFactory();
 
+        // Open a session
         Session session = sf.openSession();
 
+        // Create a new object
         Song song1 = new Song();
         song1.setSongName("songName1");
         song1.setArtist("artist1");
         System.out.println(song1);
 
+        // Save the created object in the db
         Transaction transaction = session.beginTransaction();
         session.persist(song1);
         transaction.commit();
